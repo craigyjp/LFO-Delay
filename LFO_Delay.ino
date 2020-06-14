cab2c37b9bc775fad5bd156b124320803f57f97b
craigyjp <51026305+craigyjp@users.noreply.github.com>
2020-06-14 11:10:45 +0300

    Add files via upload
    Resorted to millisec for this and it seems to work well, I can use it in gated mode or retrigger mode by monitoring the mono/multi setting from the synth and watching for key triggers on the digital pins. Delay from 0 - 10 seconds controlled by the synth CV with retriggering and +5v trigger to drive the env generator for slope.

--- LFO_Delay.ino
+++ LFO_Delay.ino
@@ -1,76 +1,73 @@
-#include <TimerOne.h>
-
-#define TRIG_OUT  4 // Trigger signal to env generator
-#define MULTI_IN  1 // active low pulse from trigger/gate of synth
-#define LED 11 // wanted to flash the led when a timer cycle had finished to show it was working mono/multi mode.
+unsigned long previousMillis = 0;
+unsigned long interval = 1; //10 seconds
+int LDRLED  = 11;            // select the input pin for the ldr
+#define TRIG_OUT  0 // Trigger signal to env generator
+#define GATE_IN  1 // active low pulse from trigger/gate of synth
+#define TRIG1 2 // test for triggers
+#define TRIG2 3 // test for triggers
+#define TRIG3 4 // test for triggers
+#define TRIG4 5 // test for triggers
+#define TRIG5 6 // test for triggers
+#define TRIG6 7 // test for triggers
+#define MONO_MULT 8 // what mode for triggers are we in
 #define DELAY_CV  A0 // Analogue signal 0-5v to determine delay time
 long delaytime  = 0;
-long newdelay = 0;
-int keytrig = 0;
+int keygate = 0;
 
-void setup() 
+void setup()
 {
-Serial.begin(19200);
-pinMode(TRIG_OUT, OUTPUT);
-pinMode(LED, OUTPUT);
-pinMode(MULTI_IN, INPUT_PULLUP);
-
-digitalWrite(TRIG_OUT, LOW);
-digitalWrite(LED, LOW);
-
-Timer1.initialize();
-Timer1.start();
-// Timer1.attachInterrupt(startLFO);
-Timer1.stop();
-}
-
-void startLFO()
-{
-  Serial.println("Starting LFO");
-  digitalWrite(LED, HIGH);
-  digitalWrite(TRIG_OUT, HIGH);
-  delayMicroseconds(100);
-  digitalWrite(LED, LOW);
-//  digitalWrite(TRIG_OUT, LOW);
-  keytrig = 1;
-}
-
-void checkTrigger()
-{
-  keytrig = digitalRead(MULTI_IN);   // read the input pin
+  pinMode(LDRLED, OUTPUT);
+  pinMode(TRIG_OUT, OUTPUT);
+  digitalWrite(TRIG_OUT, LOW);
+  pinMode(GATE_IN, INPUT_PULLUP);
+  pinMode(TRIG1, INPUT_PULLUP);
+  pinMode(TRIG2, INPUT_PULLUP);
+  pinMode(TRIG3, INPUT_PULLUP);
+  pinMode(TRIG4, INPUT_PULLUP);
+  pinMode(TRIG5, INPUT_PULLUP);
+  pinMode(TRIG6, INPUT_PULLUP);
+  pinMode(MONO_MULT, INPUT_PULLUP);
+  //Start Serial port
+  Serial.begin(9600);        // start serial for output - for testing
 }
 
 void getDelayTime()
 {
   delaytime = analogRead(DELAY_CV);
-  Serial.print("delay time ");
-  Serial.println(delaytime);
-}
-
-void timerLoop()
-{
-    Serial.println(keytrig);
-  if (keytrig == 0)
+  if (delaytime <= 0)
   {
-    Serial.print("This is what the timer sees ");
-    Serial.println(keytrig);
-    newdelay = (delaytime * 6400);
-    Serial.print("This is what the new delay time is ");
-    Serial.println(newdelay);
-    Timer1.setPeriod(newdelay); // to get upto 6.5 seconds delay
-    Timer1.start();
-//    Timer1.pwm(TRIG_OUT, 1023);
-    Timer1.pwm(TRIG_OUT, 1);
-    Timer1.disablePwm(TRIG_OUT);
-    digitalWrite(TRIG_OUT, HIGH);
-    delay(20);
-    digitalWrite(TRIG_OUT, LOW);
+    delaytime = 0.1;
   }
+  interval = (delaytime * 10);
+//  Serial.print("delay time ");
+//  Serial.println(interval);
 }
 
-void loop() 
+void loop()
 {
-  checkTrigger();
   getDelayTime();
-  timerLoop();
+  unsigned long currentMillis = millis();
+  if (digitalRead(MONO_MULT) <= 0)
+  {
+  if (digitalRead(TRIG1) <= 0 || digitalRead(TRIG2) <= 0 || digitalRead(TRIG3) <= 0 || digitalRead(TRIG4) <= 0 || digitalRead(TRIG5) <= 0 || digitalRead(TRIG1) <= 0)
+  { 
+    previousMillis = currentMillis;
+  }
+  }
+  keygate = digitalRead(GATE_IN);   // read the gate pin
+//  Serial.print("key trig ");
+//  Serial.println(keygate);
+  if (keygate <= 0) { 
+    if (currentMillis - previousMillis >= interval) 
+    {
+      digitalWrite(LDRLED, HIGH);
+      digitalWrite(TRIG_OUT, HIGH);
+    }
+  }
+  else
+  {
+    digitalWrite(LDRLED, LOW);   // set the LED off
+    digitalWrite(TRIG_OUT, LOW);
+    previousMillis = currentMillis;//reset timer so its ready for the next time
+  }
 }
